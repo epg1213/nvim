c562af344b63e8f94aaf6ac490f1c3657dbd4e14
epg1213 <epg1213@gmail.com>
2025-11-27 15:33:58 +0100

    autocomplete + iceberg theme

--- lua/epg1213/init.lua
+++ lua/epg1213/init.lua
@@ -2,7 +2,6 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
---vim.cmd.colorscheme("colorbuddy")
 
 require('after.telescope')
 require('after.colorbuddy')
--- lua/epg1213/lsp.lua
+++ lua/epg1213/lsp.lua
@@ -48,5 +48,16 @@ cmp.setup({
       vim.snippet.expand(args.body)
     end,
   },
-  mapping = cmp.mapping.preset.insert({}),
+  mapping = cmp.mapping.preset.insert({
+      
+      ['<Tab>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    }),
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
 })
