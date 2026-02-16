08ebb5b0c8d9c37f4865e17afdbc719b644c6379
epg1213 <epg1213@gmail.com>
2026-02-16 14:25:48 +0100

    treesitter fixes

---
+++ lua/after/treesitter.lua
@@ -0,0 +1,3 @@
+
+require'nvim-treesitter'.install { "python", "rust", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" }
+
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,16 +2,7 @@
 local M = {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-          ensure_installed = { "python", "rust", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
- }
+    lazy = false
+}
 
 return { M }
